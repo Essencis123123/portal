52a48d9d0c74a29b43841c2a67e42fc6988da228
Essencis123123 <earaujo@essencis.com.br>
2025-09-06 16:24:19 -0300

    Update reembolso.py

--- reembolso.py
+++ reembolso.py
@@ -10,7 +10,7 @@ import plotly.express as px
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient.discovery import build
-from googleapapi.http import MediaIoBaseUpload
+from googleapiclient.http import MediaIoBaseUpload
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
