3f45b5c04be44f1dca7a5f3181a717a52cd366b5
Essencis123123 <earaujo@essencis.com.br>
2025-09-09 13:41:34 -0300

    Update gerenciamento_reembolso.py

--- gerenciamento_reembolso.py
+++ gerenciamento_reembolso.py
@@ -536,7 +536,7 @@ if st.session_state.current_user['EMAIL'].lower() == "earaujo@essencis.com.br":
                         st.subheader("📎 Comprovante")
                         
                         try:
-                            signed_url = get_signed_url(row['ID_COMPROVANE'])
+                            signed_url = get_signed_url(row['ID_COMPROVANTE'])
                             if signed_url:
                                 if row['CAMINHO_RECIBO'].lower().endswith(('.png', '.jpg', '.jpeg')):
                                     st.image(signed_url, caption="Comprovante", use_column_width=True)
