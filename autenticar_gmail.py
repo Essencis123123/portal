6e546451ebc4eb1769b71ab2fc92ffc40c259ffd
Essencis123123 <earaujo@essencis.com.br>
2025-09-13 21:10:03 +0000

    Primeiro commit

---
+++ autenticar_gmail.py
@@ -0,0 +1,30 @@
+import os
+import pickle
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# Escopo necessário para enviar e-mails
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+            
+    service = build('gmail', 'v1', credentials=creds)
+    print("Autenticação bem-sucedida! O arquivo 'token.pickle' foi gerado.")
+
+if __name__ == '__main__':
+    main()
