54edef14d5b91681c4668261af86ca630e1a0a17
Essencis123123 <earaujo@essencis.com.br>
2026-02-20 16:56:37 -0300

    Update consulta_materiais.py

--- consulta_materiais.py
+++ consulta_materiais.py
@@ -19,7 +19,7 @@ st.title('🔍 Consulte Códigos de Materiais (Oracle Cloud)')
 
 try:
     # Carrega o arquivo Excel
-    df = pd.read_excel('codigo.xlsx')
+    df = pd.read_excel('codigos.xlsx')
     
     # Sidebar com informações
     with st.sidebar:
